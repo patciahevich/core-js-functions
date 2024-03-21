e7b2fabbd2521db8fea6f9a93360d5611cbddab5
Volha Patciahevich <zaprudskaya@gmail.com>
2024-03-21 11:19:42 +0300

    feat: solve getArgumentsCount task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,10 +50,9 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return funcs.map((func) => func.length);
 }
-
 /**
  * Returns the math power function with the specified exponent
  *
