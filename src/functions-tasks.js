54fee65b6ef3a6efe15211ea9298ca2bdf6fd120
Volha Patciahevich <zaprudskaya@gmail.com>
2024-04-04 10:56:24 +0300

    feat: solve memoize task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -120,8 +120,14 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let result;
+
+  if (!result) {
+    result = func();
+    return () => result;
+  }
+  return result;
 }
 
 /**
