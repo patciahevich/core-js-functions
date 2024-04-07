6b1528d8ced0f7b69dbd2f674979caa3012d23c3
Volha Patciahevich <zaprudskaya@gmail.com>
2024-04-07 23:11:31 +0300

    feat: solve function getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -224,8 +224,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let startIndex = startFrom;
+
+  return () => {
+    const result = startIndex;
+    startIndex += 1;
+    return result;
+  };
 }
 
 module.exports = {
